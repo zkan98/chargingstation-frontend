5cfe388bd1e7c1a50986aeaaa08e677545b3200c
zkan98 <anzk9898@gmail.com>
2024-08-16 14:59:48 +0900

    Save local changes before pull

--- src/charge/components/ChooseOne.jsx
+++ src/charge/components/ChooseOne.jsx
@@ -1,16 +1,13 @@
 import { Radio, RadioGroup, Stack } from '@chakra-ui/react';
-import { useState } from 'react';
 
-function ChooseOne() {
-  const [value, setValue] = useState('option1');
-
+function ChooseOne({ value, onChange }) {
   return (
-    <RadioGroup onChange={setValue} value={value}>
-      <Stack direction="row">
-        <Radio value="option1">일반 이용자</Radio>
-        <Radio value="option2">사업자</Radio>
-      </Stack>
-    </RadioGroup>
+      <RadioGroup onChange={onChange} value={value}>
+        <Stack direction="row">
+          <Radio value="user">일반 이용자</Radio>
+          <Radio value="business">사업자</Radio>
+        </Stack>
+      </RadioGroup>
   );
 }
 
